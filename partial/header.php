1f2da6084916116f5b2740cae7b98c768c645471
Paul Micu <paulmicu@Pauls-MacBook-Air.local>
2018-01-31 16:38:10 -0500

    view post and comment [static]
    added sigle post view with comment and subcomment. best comment has a "stamp"
    social media and likes also included for the main post.

--- partial/header.php
+++ partial/header.php
@@ -2,8 +2,12 @@
 <html>
 <head>
 	<title></title>
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<link rel="stylesheet" type="text/css" href="css/viewPost.css">
 	<link rel="stylesheet" type="text/css" href="css/bootstrap.css">
 	<script type="text/javascript" src="js/bootstrap.js"></script>
+	<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.4.0/css/font-awesome.min.css">
+	
 
 </head>
 <body>
