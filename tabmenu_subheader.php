12d5dd1bee32fa0724fc1d3d6c4a066c1f044ba5
Michelle <michelle.choi@mail.mcgill.ca>
2018-02-01 00:15:49 -0500

    tabmenu added

---
+++ tabmenu_subheader.php
@@ -0,0 +1,23 @@
+
+	<div class="tab-select" style=" margin:20px;">
+    		<ul class="nav nav-tabs">
+        		<li class="active"><a data-toggle="tab" href="#active">Active</a></li>
+        		<li><a data-toggle="tab" href="#new">New</a></li>
+        		<li><a data-toggle="tab" href="#unanswered">Unanswered</a></li>
+    		</ul>
+    		<div class="tab-content">
+        		<div id="active" class="tab-pane fade in active">
+            			<h3>Active</h3>
+            			<p>Active Questions go here</p>
+        		</div>
+        		<div id="new" class="tab-pane fade">
+            			<h3>New</h3>
+            			<p>New Questions go here</p>
+        		</div>
+        		<div id="unanswered" class="tab-pane fade">
+            			<h3>Unanswered</h3>
+            			<p>Unanswered Questions go here</p>
+        		</div>
+    		</div>
+	</div>
+
