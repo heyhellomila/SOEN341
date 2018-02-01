c7d5bc7828e77d34ed22571a98b3c93c7b726fc4
Michelle <michelle.choi@mail.mcgill.ca>
2018-02-01 15:39:24 -0500

    Update tabmenuSubheader.php

--- tabmenuSubheader.php
+++ tabmenuSubheader.php
@@ -1,14 +1,3 @@
-<!DOCTYPE html>
-<html>
-<head>
-  <meta charset="utf-8">
-  <meta name="viewport" content="width=device-width, initial-scale=1">
-  <title>Sub header tabs</title>
-  <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
-  <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.4/jquery.min.js"></script>
-  <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
-
-</head>
 
 <div class="tab-select" style="margin: 20px;">
   <!-- tabs -->
@@ -124,4 +113,4 @@
   </div>
 </div>
 </div>
-</html>
+
