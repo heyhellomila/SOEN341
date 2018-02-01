668931c245c24d9dc93bb8864fcd0dcd624dfba3
MrWorme <35611221+MrWorme@users.noreply.github.com>
2018-02-01 11:32:43 -0500

    put links in the index
    just put some temporary links in the index

--- index.php
+++ index.php
@@ -2,6 +2,8 @@
 require_once("partial/header.php");
 ?>
 
-
+ <a href="contactUS.php">contact page</a> 
+ <a href="registrationpage.php">reg page</a> 
+ <a href="viewPost.php">view post</a> 
 <?php
 require_once("partial/footer.php");
