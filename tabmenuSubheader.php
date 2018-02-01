5cfeb496972866044fadc591f3feff4fe77d1d8a
Michelle <michelle.choi@mail.mcgill.ca>
2018-02-01 14:01:23 -0500

    Update tabmenuSubheader.php
    adding tab components to main tabmenu file

--- tabmenuSubheader.php
+++ tabmenuSubheader.php
@@ -5,5 +5,20 @@
             <li class="tab item"><a data-toggle="tab" href="newquestiontab.php">New</a></li>
         	<li class="tab item"><a data-toggle="tab" href="unansweredtab.php">Unanswered</a></li>
     	</ul>
+    <div class="tab-content">
+        		<div id="active" class="tab-pane fade in active">
+            			<h3>Active</h3>
+            			<p>Active Questions go here</p>
+        		</div>
+        		<div id="new" class="tab-pane fade">
+            			<h3>New</h3>
+            			<p>New Questions go here</p>
+        		</div>
+        		<div id="unanswered" class="tab-pane fade">
+            			<h3>Unanswered</h3>
+            			<p>Unanswered Questions go here</p>
+        		</div>
+    		</div>
+	</div>
 </div>
 
