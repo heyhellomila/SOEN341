475cb253072197ea221a5b642f35c86a5600b6ae
Michelle <michelle.choi@mail.mcgill.ca>
2018-02-01 00:33:22 -0500

    Create newquestiontab.php
    queries all the newest questions

---
+++ newquestiontab.php
@@ -0,0 +1,37 @@
+<?php
+require_once("partial/header.php");?>
+    <div class="container">
+      <!-- Table of posts -->
+      <div class="my-3 p-3 bg-white">
+        <!-- Single post that is added to the homepage starts here -->
+        <h5>Question 1</h5>
+        <div class="media text-muted pt-2 mb-3 border-bottom border-gray">
+          <img alt="32x32" class="mr-2 rounded" style="width: 48px; height: 48px;" src="images/captain.png">
+          <p class="media-body pb-2 mb-3 small lh-125">
+            <strong class="d-block text-gray-dark">From: Username<span style="color: green;">&emsp;Answers: =2&emsp;</span><span style="color: blue;">Likes: 5</span></strong>
+            Donec id elit non mi porta gravida at eget metus. Fusce dapibus, tellus ac cursus commodo, tortor mauris condimentum nibh, ut fermentum massa justo sit amet risus.
+          </p>
+        </div>
+        <!-- End of single post -->
+        <h5>Question 2</h5>
+        <div class="media text-muted pt-2 mb-3 border-bottom border-gray">
+          <img alt="32x32" class="mr-2 rounded" style="width: 48px; height: 48px;" src="images/captain.png">
+          <p class="media-body pb-2 mb-3 small lh-125">
+            <strong class="d-block text-gray-dark">From: Username<span style="color: green;">&emsp;Answers: 6&emsp;</span><span style="color: blue;">Likes: 20</span></strong>
+            Donec id elit non mi porta gravida at eget metus. Fusce dapibus, tellus ac cursus commodo, tortor mauris condimentum nibh, ut fermentum massa justo sit amet risus.
+          </p>
+          <p></p>
+        </div>
+        <h5>Question 3</h5>
+        <div class="media text-muted pt-2 mb-3 border-bottom border-gray">
+          <img alt="32x32" class="mr-2 rounded" style="width: 48px; height: 48px;" src="images/captain.png">
+          <p class="media-body pb-2 mb-3 small lh-125">
+            <strong class="d-block text-gray-dark">From: Username<span style="color: green;">&emsp;Answers: 18&emsp;</span><span style="color: blue;">Likes: 43</span></strong>
+            Donec id elit non mi porta gravida at eget metus. Fusce dapibus, tellus ac cursus commodo, tortor mauris condimentum nibh, ut fermentum massa justo sit amet risus.
+          </p>
+        </div>
+      </div>
+    </div>
+    <!-- End of table -->
+<?php
+require_once("partial/footer.php");?>
