eb28b8cc671bafa316130cf243bb6b66dc402031
MrWorme <35611221+MrWorme@users.noreply.github.com>
2018-02-01 13:16:43 -0500

    Update header.php

--- partial/header.php
+++ partial/header.php
@@ -1,15 +1,35 @@
-
-<!DOCTYPE html>
+<!doctype html>
 <html>
-<head>
+
 	<title></title>
 	<meta name="viewport" content="width=device-width, initial-scale=1">
 	<link rel="stylesheet" type="text/css" href="css/viewPost.css">
 	<link rel="stylesheet" type="text/css" href="css/reg.css">
 	<link rel="stylesheet" type="text/css" href="css/bootstrap.css">
+	<link rel="stylesheet" type="text/css" href="css/header.css">
 	<script type="text/javascript" src="js/bootstrap.js"></script>
 	<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.4.0/css/font-awesome.min.css">
 
 </head>
-<body>
 
+	<body>
+		<header class = "header" id="header">
+			<div class="container" id="container">
+				<div class = "logo" id="logo">
+					<img src="images/site_logo.png" id="logopic" ></img>
+				</div>
+				<form class="search" id ="search">
+					<input class="form-control mr-sm-2" id= "searchbar" type="search" placeholder="Search" aria-label="Search"></input>
+					<button class="btn btn-outline-success" id= "searchbutton" type="submit">Search</button>
+				</form> 
+				<div class ="actions" id ="actions">
+					<div class ="notifications" id = "notifications">
+					</div>
+					<div class = "log_sign" id="log_sign">
+						<button class="login" id="login">Log In</button>
+						<button class="signup" id="signup">Sign Up</button>		
+					</div>			
+				</div>				
+			</div>
+		</header>
+	
