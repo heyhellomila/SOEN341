304e0d57db3b91d2a7f4713029ddb792b7c66056
MrWorme <micu.p@outlook.com>
2018-02-01 13:12:11 -0500

    fixed header links
    added the new links and scripts to the header

--- partial/header.php
+++ partial/header.php
@@ -1,16 +1,21 @@
 <!doctype html>
 <html>
-	<head>
-	<link rel="stylesheet" type="text/css" href="../css/header.css">
-		<title>
-		</title>
-	</head>
+	<title></title>
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<link rel="stylesheet" type="text/css" href="css/viewPost.css">
+	<link rel="stylesheet" type="text/css" href="css/reg.css">
+	<link rel="stylesheet" type="text/css" href="css/bootstrap.css">
+	<link rel="stylesheet" type="text/css" href="css/header.css">
+	<script type="text/javascript" src="js/bootstrap.js"></script>
+	<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.4.0/css/font-awesome.min.css">
+
+</head>
 
 	<body>
 		<header class = "header" id="header">
 			<div class="container" id="container">
 				<div class = "logo" id="logo">
-					<img src="../site_logo.png" id="logopic" ></img>
+					<img src="images/site_logo.png" id="logopic" ></img>
 				</div>
 				<form class="search" id ="search">
 					<input class="form-control mr-sm-2" id= "searchbar" type="search" placeholder="Search" aria-label="Search"></input>
@@ -19,7 +24,7 @@
 				<div class ="actions" id ="actions">
 					<div class ="notifications" id = "notifications">
 					</div>
-					<div class = " log_sign" id="log_sign">
+					<div class = "log_sign" id="log_sign">
 						<button class="login" id="login">Log In</button>
 						<button class="signup" id="signup">Sign Up</button>		
 					</div>			
