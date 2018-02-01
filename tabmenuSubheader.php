eb77ae2ca2bc2021277fcdc0ae8b8c7b3e45bef4
Michelle <michelle.choi@mail.mcgill.ca>
2018-02-01 13:44:48 -0500

    Update tabmenuSubheader.php

--- tabmenuSubheader.php
+++ tabmenuSubheader.php
@@ -1,9 +1,9 @@
 <div class="tab-select" style=" margin:20px;">
-    	<ul class="nav nav-tabs">
-
-   		<li class="tab"><a data-toggle="tab" href="activetab.php">Active</a></li>
-        	<li><a data-toggle="tab" href="newquestiontab.php">New</a></li>
-        	<li><a data-toggle="tab" href="unansweredtab.php">Unanswered</a></li>
+<!-- tabs -->
+    <ul class="nav nav-tabs">
+   		    <li class="tab item"><a data-toggle="tab" href="activetab.php">Active</a></li>
+            <li class="tab item"><a data-toggle="tab" href="newquestiontab.php">New</a></li>
+        	<li class="tab item"><a data-toggle="tab" href="unansweredtab.php">Unanswered</a></li>
     	</ul>
 </div>
 
