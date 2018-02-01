0c077e756ff353de827ef1de76b6f33cd2bb0b87
Bilal101 <35821680+Bilal101@users.noreply.github.com>
2018-01-31 19:21:44 -0800

    Registration/Sign up account Page And Pagelayout
    Added bootstrap 3.30 to the page , Screenshot of the page layout.

---
+++ registrationpage.php
@@ -0,0 +1,67 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+<meta charset="utf-8">
+<meta http-equiv="X-UA-Compatible" content="IE=edge">
+<meta name="viewport" content="width=device-width, initial-scale=1">
+<title>Create An Account </title>
+
+<!-- Bootstrap -->
+<link href="file:///C|/Users/ABKS/Downloads/css/bootstrap.min.css" rel="stylesheet">
+<style type="text/css">
+@import url("file:///C|/Users/ABKS/Documents/bootstrap-3.3.0/dist/css/bootstrap.css");
+@import url("file:///C|/Users/ABKS/Documents/bootstrap-3.3.0/dist/css/bootstrap.min.css");
+@import url("file:///C|/Users/ABKS/Documents/bootstrap-3.3.0/dist/css/bootstrap-theme.css");
+@import url("file:///C|/Users/ABKS/Documents/bootstrap-3.3.0/dist/css/bootstrap-theme.min.css");
+@import url("file:///C|/Users/ABKS/Documents/reg.css");
+</style>
+
+
+</head>
+<body><div class="container-fluid"  align="justify">
+	<div class="row" >
+		<h2 class="center-block border-info">Create Account </h2>
+	</div>
+</div>
+<br>
+<br>
+
+<div class="row border-0">
+<div class="col-md-4 col-md-offset-4">
+<div class="form-body">
+  
+        
+      
+    
+    <div class="tab-content">
+        <div id="sectionA" class="tab-pane fade in active">
+        <div class="innter-form">
+            <form class="sa-innate-form" method="post">
+            
+            <input type="text" name="username" placeholder="UserName" id="user">
+            <input type="text" name="emailaddress" placeholder="Email@example.com" id="email">
+            <input type="password" name="password" placeholder="Password" id="pas"><br>
+            <input type="text" name="interest" placeholder="Area Of Interest" id="Int">
+            <button type="submit" class="center-block btn-danger"  >Join Us</button>
+            
+            </form>
+            </div>
+            <div class="social-login">
+            <p>- - - - - - - - - - - - - Join Us With - - - - - - - - - - - - - </p>
+    		<ul>
+            <li><a href="" class="btn-primary"><i class="fa fa-facebook" ></i> Facebook</a></li>
+            <li><a href=""><i class="fa fa-google-plus"></i> Google+</a></li>
+            <li><a href=""><i class="fa fa-twitter"></i> Twitter</a></li>
+            </ul>
+            </div>
+            <div class="clearfix"></div>
+        </div>
+    
+    </div>
+    </div>
+    </div>
+    </div>
+    </div>
+
+</body>
+</html>
