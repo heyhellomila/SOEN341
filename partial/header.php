231ba7477c6bdfa5765ab5c1c286159e670cafc1
MrWorme <35611221+MrWorme@users.noreply.github.com>
2018-02-01 11:47:06 -0500

    fixed bilal's link
    the links were redundant and local to his machine. i changed to to be compatible with the project

--- partial/header.php
+++ partial/header.php
@@ -5,19 +5,10 @@
 	<title></title>
 	<meta name="viewport" content="width=device-width, initial-scale=1">
 	<link rel="stylesheet" type="text/css" href="css/viewPost.css">
+	<link rel="stylesheet" type="text/css" href="css/reg.css">
 	<link rel="stylesheet" type="text/css" href="css/bootstrap.css">
 	<script type="text/javascript" src="js/bootstrap.js"></script>
 	<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.4.0/css/font-awesome.min.css">
-	
-<!-- Bootstrap -->
-        <link href="file:///C|/Users/ABKS/Downloads/css/bootstrap.min.css" rel="stylesheet">
-        <style type="text/css">
-        @import url("file:///C|/Users/ABKS/Documents/bootstrap-3.3.0/dist/css/bootstrap.css");
-        @import url("file:///C|/Users/ABKS/Documents/bootstrap-3.3.0/dist/css/bootstrap.min.css");
-        @import url("file:///C|/Users/ABKS/Documents/bootstrap-3.3.0/dist/css/bootstrap-theme.css");
-        @import url("file:///C|/Users/ABKS/Documents/bootstrap-3.3.0/dist/css/bootstrap-theme.min.css");
-        @import url("file:///C|/Users/ABKS/Documents/reg.css");
-</style>
 
 </head>
 <body>
