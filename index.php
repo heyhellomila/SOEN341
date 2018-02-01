4d5f4b5e02ef34919790d566b32d342e7a92b99e
Patrick Pin <35614258+PinMeister@users.noreply.github.com>
2018-02-01 11:47:14 -0500

    Update index.php
    Added link to post table.

--- index.php
+++ index.php
@@ -5,5 +5,11 @@ require_once("partial/header.php");
  <a href="contactUS.php">contact page</a> 
  <a href="registrationpage.php">reg page</a> 
  <a href="viewPost.php">view post</a> 
+
+<?php
+require_once("Post Table.php");
+?>
+
 <?php
 require_once("partial/footer.php");
+?>
