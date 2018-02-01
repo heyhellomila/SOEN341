87db9bf170c9c968873104871b1361bafb0f580d
Bilal101 <35821680+Bilal101@users.noreply.github.com>
2018-02-01 00:55:41 -0500

    Update header.php

--- partial/header.php
+++ partial/header.php
@@ -1,3 +1,4 @@
+
 <!DOCTYPE html>
 <html>
 <head>
@@ -8,6 +9,15 @@
 	<script type="text/javascript" src="js/bootstrap.js"></script>
 	<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.4.0/css/font-awesome.min.css">
 	
+<!-- Bootstrap -->
+        <link href="file:///C|/Users/ABKS/Downloads/css/bootstrap.min.css" rel="stylesheet">
+        <style type="text/css">
+        @import url("file:///C|/Users/ABKS/Documents/bootstrap-3.3.0/dist/css/bootstrap.css");
+        @import url("file:///C|/Users/ABKS/Documents/bootstrap-3.3.0/dist/css/bootstrap.min.css");
+        @import url("file:///C|/Users/ABKS/Documents/bootstrap-3.3.0/dist/css/bootstrap-theme.css");
+        @import url("file:///C|/Users/ABKS/Documents/bootstrap-3.3.0/dist/css/bootstrap-theme.min.css");
+        @import url("file:///C|/Users/ABKS/Documents/reg.css");
+</style>
 
 </head>
 <body>
