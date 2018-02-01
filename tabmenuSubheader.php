41745daf48ed94bd6b56c15ea44f4a9630ce6b4a
Michelle <michelle.choi@mail.mcgill.ca>
2018-02-01 14:06:24 -0500

    Update tabmenuSubheader.php
    added tab content

--- tabmenuSubheader.php
+++ tabmenuSubheader.php
@@ -7,16 +7,103 @@
     	</ul>
     <div class="tab-content">
         		<div id="active" class="tab-pane fade in active">
-            			<h3>Active</h3>
-            			<p>Active Questions go here</p>
+            		<div class="container">
+      <!-- Table of posts -->
+      <div class="my-3 p-3 bg-white">
+        <!-- Single post that is added to the homepage starts here -->
+        <h5>Question 1</h5>
+        <div class="media text-muted pt-2 mb-3 border-bottom border-gray">
+          <img alt="32x32" class="mr-2 rounded" style="width: 48px; height: 48px;" src="images/captain.png">
+          <p class="media-body pb-2 mb-3 small lh-125">
+            <strong class="d-block text-gray-dark">From: Username<span style="color: green;">&emsp;Answers: 48&emsp;</span><span style="color: blue;">Likes: 89</span></strong>
+            Donec id elit non mi porta gravida at eget metus. Fusce dapibus, tellus ac cursus commodo, tortor mauris condimentum nibh, ut fermentum massa justo sit amet risus.
+          </p>
+        </div>
+        <!-- End of single post -->
+        <h5>Question 2</h5>
+        <div class="media text-muted pt-2 mb-3 border-bottom border-gray">
+          <img alt="32x32" class="mr-2 rounded" style="width: 48px; height: 48px;" src="images/dog.png">
+          <p class="media-body pb-2 mb-3 small lh-125">
+            <strong class="d-block text-gray-dark">From: Username<span style="color: green;">&emsp;Answers: 17&emsp;</span><span style="color: blue;">Likes: 168</span></strong>
+            Donec id elit non mi porta gravida at eget metus. Fusce dapibus, tellus ac cursus commodo, tortor mauris condimentum nibh, ut fermentum massa justo sit amet risus.
+          </p>
+          <p></p>
+        </div>
+        <h5>Question 3</h5>
+        <div class="media text-muted pt-2 mb-3 border-bottom border-gray">
+          <img alt="32x32" class="mr-2 rounded" style="width: 48px; height: 48px;" src="images/sakura.png">
+          <p class="media-body pb-2 mb-3 small lh-125">
+            <strong class="d-block text-gray-dark">From: Username<span style="color: green;">&emsp;Answers: 9&emsp;</span><span style="color: blue;">Likes: 10</span></strong>
+            Donec id elit non mi porta gravida at eget metus. Fusce dapibus, tellus ac cursus commodo, tortor mauris condimentum nibh, ut fermentum massa justo sit amet risus.
+          </p>
+        </div>
+      </div>
         		</div>
         		<div id="new" class="tab-pane fade">
-            			<h3>New</h3>
-            			<p>New Questions go here</p>
+            			<div class="container">
+      <!-- Table of posts -->
+      <div class="my-3 p-3 bg-white">
+        <!-- Single post that is added to the homepage starts here -->
+        <h5>Question 1</h5>
+        <div class="media text-muted pt-2 mb-3 border-bottom border-gray">
+          <img alt="32x32" class="mr-2 rounded" style="width: 48px; height: 48px;" src="images/captain.png">
+          <p class="media-body pb-2 mb-3 small lh-125">
+            <strong class="d-block text-gray-dark">From: Username<span style="color: green;">&emsp;Answers: =2&emsp;</span><span style="color: blue;">Likes: 5</span></strong>
+            Donec id elit non mi porta gravida at eget metus. Fusce dapibus, tellus ac cursus commodo, tortor mauris condimentum nibh, ut fermentum massa justo sit amet risus.
+          </p>
+        </div>
+        <!-- End of single post -->
+        <h5>Question 2</h5>
+        <div class="media text-muted pt-2 mb-3 border-bottom border-gray">
+          <img alt="32x32" class="mr-2 rounded" style="width: 48px; height: 48px;" src="images/dog.png">
+          <p class="media-body pb-2 mb-3 small lh-125">
+            <strong class="d-block text-gray-dark">From: Username<span style="color: green;">&emsp;Answers: 6&emsp;</span><span style="color: blue;">Likes: 20</span></strong>
+            Donec id elit non mi porta gravida at eget metus. Fusce dapibus, tellus ac cursus commodo, tortor mauris condimentum nibh, ut fermentum massa justo sit amet risus.
+          </p>
+          <p></p>
+        </div>
+        <h5>Question 3</h5>
+        <div class="media text-muted pt-2 mb-3 border-bottom border-gray">
+          <img alt="32x32" class="mr-2 rounded" style="width: 48px; height: 48px;" src="images/sakura.png">
+          <p class="media-body pb-2 mb-3 small lh-125">
+            <strong class="d-block text-gray-dark">From: Username<span style="color: green;">&emsp;Answers: 18&emsp;</span><span style="color: blue;">Likes: 43</span></strong>
+            Donec id elit non mi porta gravida at eget metus. Fusce dapibus, tellus ac cursus commodo, tortor mauris condimentum nibh, ut fermentum massa justo sit amet risus.
+          </p>
+        </div>
+      </div>
         		</div>
         		<div id="unanswered" class="tab-pane fade">
-            			<h3>Unanswered</h3>
-            			<p>Unanswered Questions go here</p>
+            			<div class="container">
+      <!-- Table of posts -->
+      <div class="my-3 p-3 bg-white">
+        <!-- Single post that is added to the homepage starts here -->
+        <h5>Question 1</h5>
+        <div class="media text-muted pt-2 mb-3 border-bottom border-gray">
+          <img alt="32x32" class="mr-2 rounded" style="width: 48px; height: 48px;" src="images/sakura.png">
+          <p class="media-body pb-2 mb-3 small lh-125">
+            <strong class="d-block text-gray-dark">From: flower<span style="color: green;">&emsp;Answers: 0&emsp;</span><span style="color: blue;">Likes: 125</span></strong>
+            Donec id elit non mi porta gravida at eget metus. Fusce dapibus, tellus ac cursus commodo, tortor mauris condimentum nibh, ut fermentum massa justo sit amet risus.
+          </p>
+        </div>
+        <!-- End of single post -->
+        <h5>Question 2</h5>
+        <div class="media text-muted pt-2 mb-3 border-bottom border-gray">
+          <img alt="32x32" class="mr-2 rounded" style="width: 48px; height: 48px;" src="images/dog.png">
+          <p class="media-body pb-2 mb-3 small lh-125">
+            <strong class="d-block text-gray-dark">From: amazingtrainer<span style="color: green;">&emsp;Answers: 0&emsp;</span><span style="color: blue;">Likes: 125</span></strong>
+            Donec id elit non mi porta gravida at eget metus. Fusce dapibus, tellus ac cursus commodo, tortor mauris condimentum nibh, ut fermentum massa justo sit amet risus.
+          </p>
+          <p></p>
+        </div>
+        <h5>Question 3</h5>
+        <div class="media text-muted pt-2 mb-3 border-bottom border-gray">
+          <img alt="32x32" class="mr-2 rounded" style="width: 48px; height: 48px;" src="images/captain.png">
+          <p class="media-body pb-2 mb-3 small lh-125">
+            <strong class="d-block text-gray-dark">From: Username<span style="color: green;">&emsp;Answers: 0&emsp;</span><span style="color: blue;">Likes: 125</span></strong>
+            Donec id elit non mi porta gravida at eget metus. Fusce dapibus, tellus ac cursus commodo, tortor mauris condimentum nibh, ut fermentum massa justo sit amet risus.
+          </p>
+        </div>
+      </div>
         		</div>
     		</div>
 	</div>
