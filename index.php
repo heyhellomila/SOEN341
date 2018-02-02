1347ae5c1138e7d29abd3181923a59ffe2a88736
Paul Micu <paulmicu@pauls-air.wireless.concordia.ca>
2018-02-02 09:45:23 -0500

    fixed signin again
    for some reason the code keeps comming back so i'm errasing it again

--- index.php
+++ index.php
@@ -1,7 +1,6 @@
 <?php 
 require_once("partial/header.php") 
 ?>
-<<<<<<< HEAD
 
  <a href="contactUS.php">contact page</a> 
  <a href="registrationpage.php">reg page</a> 
@@ -14,46 +13,3 @@ require_once("Post Table.php");
 <?php
 require_once("partial/footer.php");
 ?>
-=======
-	<div  class="text-center">
-	<form class="form-signin">
-		<h1 class="h3 mb-3 font-weight-normal">Please sign in</h1>
-		<label for="inputEmail" class="sr-only">Email address</label>
-		<input type="email" id="inputEmail" class="form-control" placeholder="Username/Email address" required autofocus>
-		<label for="inputPassword" class="sr-only">Password</label>
-		<input type="password" id="inputPassword" class="form-control" placeholder="Password" required>
-		<div class="checkbox mb-3">
-			<label>
-				<input type="checkbox" value="remember-me"> Remember me
-			</label>
-		</div>
-		<button class="btn btn-lg btn-primary btn-block" type="submit">
-		Sign in
-		</button>
-		<button class="btn btn-lg btn-block facebook-button-colour"  type="submit">
-			<i class="fa fa-facebook"></i>
-			Sign in with Facebook
-			
-		</button>
-
-		<button class="btn btn-lg btn-block google-button-colour"  type="submit">
-			<i class="fa fa-google"></i>
-			Sign in with Google
-			
-		</button>
-		
-
-
-
-		<hr>
-
-			<div class="form-group">
-				<a href="#forgot" data-toggle="modal"> Forgot Password? </a>
-			</div>
-		
-	</form>
-</div>
-<?php 
-require_once("partial/footer.php") 
-?>
->>>>>>> signinbranch
--- partial/header.php
+++ partial/header.php
@@ -1,27 +1,24 @@
 <!doctype html>
 <html>
 
-
-<title></title>
-<meta name="viewport" content="width=device-width, initial-scale=1">
-<link rel="stylesheet" type="text/css" href="css/viewPost.css">
-<link rel="stylesheet" type="text/css" href="css/reg.css">
-<link rel="stylesheet" type="text/css" href="css/bootstrap.css">
-<script type="text/javascript" src="js/bootstrap.js"></script>
-<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.4.0/css/font-awesome.min.css">
-
 <head>
 	<title></title>
-	<link rel="stylesheet" type="text/css" href="css/bootstrap.css">
-	<script type="text/javascript" src="js/bootstrap.js"></script>
-		<link rel="stylesheet" type="text/css" href="css/SignIn.css">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<link rel="stylesheet" type="text/css" href="css/viewPost.css">
+	<link rel="stylesheet" type="text/css" href="css/reg.css">
+	<link rel="stylesheet" type="text/css" href="css/SignIn.css">
+	<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
+
+	<script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js" integrity="sha384-JZR6Spejh4U02d8jOt6vLEHfe/JQGiRRSQQxSfFWpi1MquVdAyjUar5+76PVCmYl" crossorigin="anonymous"></script>
 	<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.4.0/css/font-awesome.min.css">
+
+
 
 
 </head>
 
 <body>
-	<nav class="navbar sticky-top  navbar-dark bg-dark justify-content-between">
+	<nav class="navbar sticky-top navbar-dark bg-dark justify-content-between">
 		<a class="navbar-brand col pt-0" href="index.php">
 			<img class="navbar-brand " src="images/site_logo.png" height="60">
 		</a>
@@ -32,10 +29,10 @@
 			
 		</form>
 		<div class="col">
-		<form class="form-inline row">
-			<a class="mr-2" href="SignIn.php"><button class="btn btn-secondary " type="button">Sign In</button></a>
-			<a class="mr-2" href="registrationpage.php"><button class="btn btn-primary" type="button">Sign Up</button></a>
-		</form>
+			<form class="form-inline row">
+				<a class="mr-2" href="SignIn.php"><button class="btn btn-secondary " type="button">Sign In</button></a>
+				<a class="mr-2" href="registrationpage.php"><button class="btn btn-primary" type="button">Sign Up</button></a>
+			</form>
 		</div>
 	</nav>
 	
