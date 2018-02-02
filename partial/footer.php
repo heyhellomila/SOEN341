7ba55f2554b7e92de2f489b88e762debf76338b4
Mila Roisin <mila.roisin@gmail.com>
2018-02-02 12:24:38 -0500

    Code for Footer (Placeholder)
    Apache file will be included in next sprint

--- partial/footer.php
+++ partial/footer.php
@@ -1,2 +1,9 @@
+<meta charset="UTF-8">
+<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
+<html>
+<body>
+<div id="footer", class="footer navbar-fixed-bottom">
+    PLACEHOLDER FOR LINKS
+</div>
 </body>
 </html>
