94b09971e785b5aeff8bda7f91c7febab295cfc8
Paul Micu <paulmicu@pauls-air.wireless.concordia.ca>
2018-02-02 09:30:33 -0500

    fixed index and header
    reverted accidental changes to those files

--- partial/header.php
+++ partial/header.php
@@ -1,12 +1,33 @@
-<!DOCTYPE html>
+<!doctype html>
 <html>
-<head>
-	<title></title>
-	<link rel="stylesheet" type="text/css" href="css/bootstrap.css">
-	<script type="text/javascript" src="js/bootstrap.js"></script>
-		<link rel="stylesheet" type="text/css" href="css/SignIn.css">
-	<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.4.0/css/font-awesome.min.css">
+
+<title></title>
+<meta name="viewport" content="width=device-width, initial-scale=1">
+<link rel="stylesheet" type="text/css" href="css/viewPost.css">
+<link rel="stylesheet" type="text/css" href="css/SignIn.css">
+<link rel="stylesheet" type="text/css" href="css/reg.css">
+<link rel="stylesheet" type="text/css" href="css/bootstrap.css">
+<script type="text/javascript" src="js/bootstrap.js"></script>
+<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.4.0/css/font-awesome.min.css">
 
 </head>
-<body>
 
+<body>
+	<nav class="navbar sticky-top  navbar-dark bg-dark justify-content-between">
+		<a class="navbar-brand col pt-0" href="index.php">
+			<img class="navbar-brand " src="images/site_logo.png" height="60">
+		</a>
+
+		<form class="form-inline col-8">
+			<input class="form-control mr-sm-3 col" type="search" placeholder="Search" aria-label="Search">
+			<button class="btn btn-outline-success" type="submit">Search</button>
+			
+		</form>
+		<div class="col">
+		<form class="form-inline row">
+			<a class="mr-2" href="SignIn.php"><button class="btn btn-secondary " type="button">Sign In</button></a>
+			<a class="mr-2" href="registrationpage.php"><button class="btn btn-primary" type="button">Sign Up</button></a>
+		</form>
+		</div>
+	</nav>
+	
