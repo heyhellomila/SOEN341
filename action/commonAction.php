69e04c6c6ab1734a5a58f633a33e1c34b1f71cf2
MrWorme <micu.p@outlook.com>
2018-02-10 15:23:55 -0500

    added commonaction
    commonaction is the php cornerstone of the backend. it executes every time a php file is loaded and sets some session file. it also manages pages visibility it must be incuded at the top of every action.php file as it is and abstract methode. the function executeAction must be defined in all files. this methode executes after visibility and session check

--- action/commonAction.php
+++ action/commonAction.php
@@ -1,1 +1,54 @@
-commonAction.php
+<?php
+	// start session must be the first line of code
+	session_start();
+
+	// import the constant file and the database connection file.
+	require_once("action/constants.php");
+	require_once("action/dba/connection.php");
+
+
+	abstract class CommonAction {
+		public static $VISIBILITY_PUBLIC = 0;
+		public static $VISIBILITY_MEMBER=1;
+		public static $VISIBILITY_ADMINISTRATOR = 10;
+
+
+		private $pageVisibility;
+
+		// constructor
+		public function __construct($pageVisibility) {
+			$this->pageVisibility = $pageVisibility;
+		}
+
+		public function execute() {
+			// if the user clicked logout kill the current sessiona and start a new one
+			if (!empty($_GET["logout"])) {
+				session_unset();
+				session_destroy();
+				session_start();
+			}
+			//if its the first time someone goes to the website in a session. assign him the public visibility. this only execute once every session
+			if (empty($_SESSION["visibility"])) {
+				$_SESSION["visibility"] = commonAction::$VISIBILITY_PUBLIC;
+			}
+			// if the visibility of this page is bigger than the session/viewer visibility redirect to index.php
+			if ($_SESSION["visibility"] < $this->pageVisibility) {
+				header("location:index.php");				
+				exit;
+			}
+
+			// execute function is abastract and must be defined in all the action.php files
+			$this->executeAction();
+		}
+
+		protected abstract function executeAction();
+
+		// function checks if the session visibility is higher than public which means that someone is loged in cuz pupblic = 0 and member = 1
+		public function isLoggedIn() {
+			return $_SESSION["visibility"] > CommonAction::$VISIBILITY_PUBLIC;
+		}
+
+		
+	}
+
+	
