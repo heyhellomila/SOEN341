f16776c75e22ee27f6f64f6a41b417d008f80a7c
Paul Micu <paulmicu@Pauls-MacBook-Air.local>
2018-02-14 11:04:26 -0500

    added 3 mysql requests
    added get post, get post creator and get comments in the mysqlrequest file.

--- action/dba/MySQLrequests.php
+++ action/dba/MySQLrequests.php
@@ -41,3 +41,53 @@
 			//return your variable
 			return $info;
 		}
+
+
+		public static function getPostbyID($id) {
+			$connection = Connection::getConnection();
+
+			$statement = $connection->prepare("SELECT * from post where post_id=?;");
+			
+			$statement->bindParam(1, $id);
+			$statement->execute();
+
+			$statement->setFetchMode(PDO::FETCH_ASSOC);
+			$info = $statement->fetch();
+
+			Connection::closeConnection();
+			return $info;
+		}
+
+		public static function getPostCreatorByPostID($id) {
+			$connection = Connection::getConnection();
+
+			$statement = $connection->prepare("SELECT user.* from user left JOIN  post ON (user_id = post.post_creator) where post.post_id=?;");
+			
+			$statement->bindParam(1, $id);
+			$statement->execute();
+
+			$statement->setFetchMode(PDO::FETCH_ASSOC);
+			$info = $statement->fetch();
+			
+			Connection::closeConnection();
+			return $info;
+		
+		}
+
+		public static function getCommentsByPostID($id) {
+			$connection = Connection::getConnection();
+
+			$statement = $connection->prepare("SELECT comment.* from comment,post_comment_ass where comment.comment_id=post_comment_ass.comment_id and post_comment_ass.post_id=?;");
+			
+			$statement->bindParam(1, $id);
+			$statement->execute();
+
+			$statement->setFetchMode(PDO::FETCH_ASSOC);
+			$info = $statement->fetch();
+			
+			Connection::closeConnection();
+			return $info;
+		
+		}
+
+	}
