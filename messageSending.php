aed89caf667b9591dc7db3064330db6c9d5c1da2
Landry <landrysigni@yahoo.com>
2018-02-14 00:10:38 -0500

    message confirmation page
    adding a message sending confirmation page  page

---
+++ messageSending.php
@@ -0,0 +1,18 @@
+<?php
+require_once("partial/header.php");
+?>
+
+<div class="container">
+        <div class="row">
+            <div class="col-sm-12 col-lg-12">
+                <h1 class="h1">Contact us </h1>
+                <p class="bd-lead">Your message has been sent</p>
+            </div>
+        </div>
+    </div>
+
+<?php
+require_once("partial/footer.php");
+?>
+
+
