6e2436ae2435ac831d08b543c94a2746ff7d2de3
miche-cho <michelle.choi@mail.mcgill.ca>
2018-02-16 05:50:05 -0500

    query posts from database onto tabmenu
    Depending on what tab is clicked on, the server requests a query through the database for the relevant posts (most liked, newest, and least liked).
    
    some problems were run into (had difficulty querying the post creator - which could be done through querying the  PostID for the post_creator).
    
    Will consult on how to query the number of replies given to post in order to finish the unanswered tab (it is filled by querying the leasted liked posts at the moment).

--- tabmenu.php
+++ tabmenu.php
@@ -1,3 +1,21 @@
+<?php
+
+//make connection
+$conn=mysqli_connect('localhost','root','');
+//select db
+mysqli_select_db($conn,'341');
+$sql="SELECT * FROM post ORDER BY post_nb_likes DESC LIMIT 3";
+$sql2="SELECT * FROM post ORDER BY post_id DESC LIMIT 3";
+$sql3="SELECT * FROM post ORDER BY post_id ASC LIMIT 3";
+
+$liked=$conn->query($sql);
+$newest=$conn->query($sql2);
+$unpopular=$conn->query($sql3);
+//call fetch_assoc() method of result object
+//fetch_assoc() will automatically keep fetching next row when called again
+
+
+?>
 <div id="accordion">
 <div class="container bg-white px-0" style="line-height: 0.5 !important;" `>
 	<!-- Nav tabs -->
@@ -20,36 +38,20 @@
 				<!-- Table of posts -->
 				<div >
 					<!-- Single post that is added to the homepage starts here -->
+					<?php while ($post = $liked->fetch_assoc()): ?>
 					<div class="row border-bottom border-gray">    
 						<div class="col">
-							<h5 class="row my-1">Aon mi porta gravida at eget metus. Fusce dapibus, tellus ac cursus commodo ?</h5>
+							<h5 class="row my-1"><?=$post["post_title"]?></h5>
 							<div class="my-1 text-muted small row">
 								<p class="m-0">
-									<strong class="text-gray-dark">From: Username<span style="color: green;">&emsp;Answers: 70&emsp;</span><span style="color: blue;">Likes: 26</span>&emsp;Posted: 2 hours ago</strong>
+									<strong class="text-gray-dark">From: <?=$post["post_creator"]?><span style="color: green;">&emsp;Answers: 70&emsp;</span><span style="color: blue;">Likes: <?=$post["post_nb_likes"]?></span>&emsp;Posted: <?=$post["post_creation_time"]?></strong>
 								</p>
 							</div>
 						</div>
 					</div>
+					<?php endwhile; ?>
 					<!-- End of single post -->
-					<div class="row border-bottom border-gray">
-						<div class="col">
-							<h5 class="row my-1">Non mi porta gravida at eget metus. Fusce dapibus?</h5>
-							<div class="my-1 text-muted small row">
-								<p class="m-0">
-									<strong class="d-block text-gray-dark">From: Username<span style="color: green;">&emsp;Answers: 6&emsp;</span><span style="color: blue;">Likes: 20</span>&emsp;Posted: 1 day ago</strong>
-								</p>
-							</div>
-						</div>
-					</div>
-					<div class="row border-bottom border-gray">
-						<div class="col">
-							<h5 class="row my-1">Non mi porta gravida at eget metus?</h5>
-							<div class="my-1 text-muted row">
-								<p class="small m-0">
-									<strong class="d-block text-gray-dark">From: Username<span style="color: green;">&emsp;Answers: 18&emsp;</span><span style="color: blue;">Likes: 43</span>&emsp;Posted: 12 hours ago</strong>
-								</p>
-							</div></div>
-						</div>
+					
 					</div>
 				</div>
 			</div>
@@ -58,32 +60,18 @@
 					<!-- Table of posts -->
 					<div>
 						<!-- Single post that is added to the homepage starts here -->
+						<?php while ($post = $newest->fetch_assoc()): ?>
 						<div class="row border-bottom border-gray">
 							<div class="col">
-								<h5 class="row">Non mi porta gravida at eget metus. Fusce dapibus, tellus ac cursus?</h5>
+								<h5 class="row"><?=$post["post_title"]?></h5>
 								<div class="my-1 text-muted row">
 									<p class="small m-0">
-										<strong class="d-block text-gray-dark">From: Username<span style="color: green;">&emsp;Answers: 2&emsp;</span><span style="color: blue;">Likes: 5</span>&emsp;Posted: Just now</strong>
+										<strong class="d-block text-gray-dark">From: <?=$post["post_creator"]?><span style="color: green;">&emsp;Answers: 2&emsp;</span><span style="color: blue;">Likes: <?=$post["post_nb_likes"]?></span>&emsp;Posted:<?=$post["post_creation_time"]?></strong>
 									</p>
 								</div></div>
 							</div>
+							<?php endwhile; ?>
 							<!-- End of single post -->
-							<div class="row border-bottom border-gray"><div class="col">
-								<h5 class="row">Fusce dapibus, tellus ac cursus commodo?</h5>
-								<div class="my-1 text-muted row">
-									<p class="small m-0">
-										<strong class="d-block text-gray-dark">From: Username<span style="color: green;">&emsp;Answers: 6&emsp;</span><span style="color: blue;">Likes: 20</span>&emsp;Posted: 15 minutes ago</strong>
-									</p>
-								</div></div>
-							</div>
-							<div class="row border-bottom border-gray"><div class="col">
-								<h5 class="row">Non mi porta gravida at eget metus. Tellus ac cursus commodo?</h5>
-								<div class="my-1 text-muted row">
-									<p class="small m-0">
-										<strong class="d-block text-gray-dark">From: Username<span style="color: green;">&emsp;Answers: 18&emsp;</span><span style="color: blue;">Likes: 43</span>&emsp;Posted: 1 hour ago</strong>
-									</p>
-								</div></div>
-							</div>
 						</div>
 					</div>
 				</div>
@@ -92,33 +80,18 @@
 						<!-- Table of posts -->
 						<div>
 							<!-- Single post that is added to the homepage starts here -->
+							<?php while ($post = $unpopular->fetch_assoc()): ?>
 							<div class="row border-bottom border-gray"><div class="col">
-								<h5 class="row">Non mi porta gravida at eget metus?</h5>
+								<h5 class="row"><?=$post["post_title"]?></h5>
 								<div class="my-1 text-muted row">
 									<p class="small m-0">
-										<strong class="d-block text-gray-dark">From: flower<span style="color: green;">&emsp;Answers: 0&emsp;</span><span style="color: blue;">Likes: 125</span>&emsp;Posted: 15 hours ago</strong>
+										<strong class="d-block text-gray-dark">From: <?=$post["post_creator"]?><span style="color: green;">&emsp;Answers: 0&emsp;</span><span style="color: blue;">Likes: <?=$post["post_nb_likes"]?></span>&emsp;Posted: <?=$post["post_creation_time"]?></strong>
 									</p>
 								</div></div>
 							</div>
+							<?php endwhile; ?>
+
 							<!-- End of single post -->
-							<div class="row border-bottom border-gray"><div class="col">
-								<h5 class="row">Gravida at eget metus. Fusce dapibus, tellus ac cursus commodo?</h5>
-								<div class="my-1 text-muted row">
-									<p class="small m-0">
-										<strong class="d-block text-gray-dark">From: amazingtrainer<span style="color: green;">&emsp;Answers: 0&emsp;</span><span style="color: blue;">Likes: 125</span>&emsp;Posted: 3 days ago</strong>
-									</p>
-								</div></div>
-								<p></p>
-							</div>
-							<div class="row border-bottom border-gray"><div class="col">
-								<h5 class="row">Porta gravida at eget metus. Fusce dapibus, tellus ac cursus commodo?</h5>
-								<div class="my-1 text-muted row">
-									<p class="small m-0">
-										<strong class="d-block text-gray-dark">From: Username<span style="color: green;">&emsp;Answers: 0&emsp;</span><span style="color: blue;">Likes: 125</span>&emsp;Posted: 5 hours ago</strong>
-									</p>
-								</div>
-							</div>
-						</div>
 					</div>
 				</div>
 			</div>
