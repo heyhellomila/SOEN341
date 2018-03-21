6982a0bdf8b2c32eb390d2b103e99aa6ee62a083
Bilal101 <35821680+Bilal101@users.noreply.github.com>
2018-03-21 13:43:22 -0400

    Added action register file
    Change the registration page code to class format.
    moved the file from main folder to action folder .
    Transform the code so it can work with registration page .

---
+++ action/registerAction.php
@@ -0,0 +1,43 @@
+<?php
+
+require_once("action/commonAction.php");
+require_once("action/dba/MySQLrequests.php");
+
+class SignUpAction extends commonAction {
+
+	public $wrongSignUp;
+     public $GoodSignUp;
+
+	public function __construct() {
+		parent::__construct(commonAction::$VISIBILITY_PUBLIC);
+		
+	}
+
+	protected function executeAction() {
+		$this->wrongSignUp = false;
+           $this->GoodSignUp = false;
+
+
+                  if(isset($_REQUEST['username']) && isset($_REQUEST['pwd1']) && isset($_REQUEST['email']) )
+{
+	$check =MySQLrequests::checkEmail($_REQUEST['email']);
+	if($check-> rowCount() > 0)
+	{ 
+		$this->wrongSignUp = true;
+	}
+	else {
+		MySQLrequests::signup($_REQUEST['username'],$_REQUEST['email'],$_REQUEST['pwd1']);
+
+		$this->GoodSignUp = true;
+	}
+}
+
+
+
+
+
+
+
+
+	}
+}
