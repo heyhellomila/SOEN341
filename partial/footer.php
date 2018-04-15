4c1ff7c484c44ea7af57b5e6db1f1fba6451f357
Paul Micu <paulmicu@Pauls-MacBook-Air.local>
2018-04-15 17:25:58 -0400

    partial refactoring

--- partial/footer.php
+++ partial/footer.php
@@ -2,11 +2,11 @@
 <div id="footer" class="footer navbar navbar-dark bg-dark fixed-bottom">
 	<div id="topPage"><a href="#top"><img  src="./images/up.png"></a></div>
     <div class="container" >
-        <form class="row mx-auto" method="post" action="postQuestion.php">
+        <form class="row mx-auto" method="post" action="PostQuestion.php">
             <button class="btn btn-outline-success" type="submit">Got a Question? Click here to ask!</button>        
         </form>
         <div class="row text-white">
-            <a class="text-white" href="contactus.php"> CONTACT US  </a> |  <a class="text-white" href="legal.php"> LEGAL </a> |  <a class="text-white" href="Privacy_policy.php"> PRIVACY  </a>
+            <a class="text-white" href="ContactUs.php"> CONTACT US  </a> |  <a class="text-white" href="Legal.php"> LEGAL </a> |  <a class="text-white" href="PrivacyPolicy.php"> PRIVACY  </a>
         </div>
     </div>
     
