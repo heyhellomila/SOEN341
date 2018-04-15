bc7974747f0437f825fb58f19e47c973690d3f9a
Patrick Pin <35614258+PinMeister@users.noreply.github.com>
2018-04-15 18:09:31 -0400

    Update and rename profilePage.php to ProfilePage.php
    Refactor code and rename file to conform to naming conventions.

--- ProfilePage.php
+++ ProfilePage.php
@@ -19,11 +19,11 @@ require_once("partial/Header.php")
 			<div class="panel panel-default ml-2">
 
 				<div class="row"><strong>Username: </strong>
-					<?=$_SESSION["userInfo"]["user_name"]?>
+					<?=$_SESSION["user_info"]["user_name"]?>
 				</div>
 				<div class="row">
 					<strong>Member Since: </strong>
-					<?=$_SESSION["userInfo"]["user_creation_time"]?>
+					<?=$_SESSION["user_info"]["user_creation_time"]?>
 				</div>
 				<div class="row"><strong>Number of Posts: </strong>
 					<?=$action->getNumberPosts($_SESSION["user_id"])?>
@@ -36,7 +36,7 @@ require_once("partial/Header.php")
 				<div class="panel-body">
 					<div align="center">
 						<div class="col-lg-12 col-md-12">
-							<img class="img img-responsive" src="images/icons/<?=$_SESSION["userInfo"]["user_img"]?>" >
+							<img class="img img-responsive" src="images/icons/<?=$_SESSION["user_info"]["user_img"]?>" >
 						</div>
 					</div>
 				</div>
@@ -47,14 +47,11 @@ require_once("partial/Header.php")
 			<div class="panel panel-default mb-2">
 				<div class="panel-body">
 <br>
-					<a href="editProfilePage.php"><button class="btn btn-primary btn-sm"> Edit Profile</button></a>
+					<a href="editProfilePage.php"><button class="btn btn-primary btn-sm">Edit Profile</button></a>
 				</div>
 			</div>
 		</div>
 		<hr>
-
-
-
 	</div>
 </div>
 </div>
