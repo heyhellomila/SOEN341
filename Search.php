6bb2fcd9b9a44e6b4500de981af4c2045b56be83
Patrick Pin <35614258+PinMeister@users.noreply.github.com>
2018-04-15 18:16:55 -0400

    Update and rename search.php to Search.php
    Rename file to conform to naming conventions.

--- Search.php
+++ Search.php
@@ -1,5 +1,3 @@
-
-
 <?php
 require_once("action/SignInAction.php");
 
@@ -8,7 +6,6 @@ $action = new SignInAction();
 $action->execute();
 
 require_once("partial/Header.php");
-
 
 $connection = Connection::getConnection();
 
@@ -22,31 +19,20 @@ if (isset($_REQUEST['query'])) {
     if ($check->rowCount() > 0) {
         echo "<h2>Search Result</h2>";
         while ($results = $check->fetch(PDO::FETCH_ASSOC)) {
-            
-            
-            
-            
-            
-            
-            
-            
+
             echo "<form name=\"title\" action=\"index.php\" method=\"post\">";
             echo "<input type=\"hidden\" name=\"post_id\" value=\"" . $results['post_id'] . "\"></input>";
-            
-            
-            
+
             echo "<button type=\"submit\" class=\"notButton\">" . $results['post_title'] . "</button></form>";
             
             echo "<p>&nbsp&nbsp&nbsp<strong> Posted:</strong>" . $results['post_creation_time'] . "</p>";
             
             echo "<p>&nbsp&nbsp&nbsp" . $results['post_content'] . "</p>";
-            
-            
-            
+
         }
     } else {
         
-        echo "No results found ";
+        echo "No results found.";
     }
     
     
@@ -59,10 +45,7 @@ else {
     echo "No results found ";
 }
 
-
-
 ?>
 <?php
 require_once("partial/Footer.php");
 ?>
-
